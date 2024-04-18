6b0991d558e47cc3b19031fb4d49ea58458c9b3f
teodorSmilevski <teosmilevski@outlook.com>
2024-04-19 01:33:31 +0200

    Landing Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
+import bookImg from "../assets/books-demo.png";
 import "../components/HomePage.css";
 export default function HomePage() {
   return (
     <section id="landing-page">
-      <div className="lp-text">
-        <h1>Heading</h1>
-        <p>small instruction</p>
-        <h5>button</h5>
+      <div id="lp-div">
+        <div className="lp-text">
+          <h1>Discover worlds between the pages</h1>
+          {/* <p>Unlock the magic of books at your fingertips</p> */}
+          <span> Discover today!</span>
+        </div>
+        <div className="lp-image">
+          <img src={bookImg} alt="" />
+        </div>
       </div>
-      <div className="lp-image">slika</div>
     </section>
   );
 }
