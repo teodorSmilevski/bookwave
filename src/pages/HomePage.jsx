9be4f080b84276fd7376f6d83d2580b2c00de010
teodorSmilevski <teosmilevski@outlook.com>
2024-04-19 01:14:38 +0200

    LandingPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
+import "../components/HomePage.css";
 export default function HomePage() {
   return (
-    <div>
-      <h1>LOLO</h1>
-    </div>
+    <section id="landing-page">
+      <div className="lp-text">
+        <h1>Heading</h1>
+        <p>small instruction</p>
+        <h5>button</h5>
+      </div>
+      <div className="lp-image">slika</div>
+    </section>
   );
 }
