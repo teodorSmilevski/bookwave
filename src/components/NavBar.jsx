71543bde5afd4e5127b13e8aa75e662fc5a4b35d
teodorSmilevski <teosmilevski@outlook.com>
2024-04-22 23:27:36 +0200

    NavBar changed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import logo from "../assets/bookwave-logo.png";
 import { Link, NavLink } from "react-router-dom";
 import "./NavBar.css";
 import { useState } from "react";
@@ -9,39 +10,41 @@ export default function NavBar() {
   }
   return (
     <nav id="navbar">
-      <Link to="/ " className="nav-links">
-        <h1>Bookwave</h1>
-      </Link>
-      <div className="dropdown-menu" onClick={handleMenuClick}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <NavLink to="/new-books" className="nav-links">
-            Whats new?
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/categories" className="nav-links">
-            Categories
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/bestsellers" className="nav-links">
-            Bestsellers
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/comics" className="nav-links comic-link">
-            Comics
-          </NavLink>
-        </li>
-      </ul>
-      <div className="nav-search-cart">
-        <p>search bar</p>
-        <p>cart</p>
+      <div>
+        <Link to="/ " className="nav-links">
+          <img src={logo} alt="" />
+        </Link>
+        <div className="dropdown-menu" onClick={handleMenuClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <NavLink to="/new-books" className="nav-links">
+              Whats new?
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/categories" className="nav-links">
+              Categories
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/bestsellers" className="nav-links">
+              Bestsellers
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/comics" className="nav-links comic-link">
+              Comics
+            </NavLink>
+          </li>
+        </ul>
+        <div className="nav-search-cart">
+          <p>search bar</p>
+          <p>cart</p>
+        </div>
       </div>
     </nav>
   );
