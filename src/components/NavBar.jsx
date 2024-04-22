82a7ed9f129623f01be20ceeda4f75ce2b8157f0
teodorSmilevski <teosmilevski@outlook.com>
2024-04-22 23:58:38 +0200

    Finished Header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,41 +9,43 @@ export default function NavBar() {
     setMenuOpen((old) => !old);
   }
   return (
-    <nav id="navbar">
-      <Link to="/ " className="nav-links">
-        <img src={logo} alt="" />
-      </Link>
-      <div className="dropdown-menu" onClick={handleMenuClick}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <NavLink to="/new-books" className="nav-links">
-            Whats new?
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/categories" className="nav-links">
-            Categories
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/bestsellers" className="nav-links">
-            Bestsellers
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/comics" className="nav-links comic-link">
-            Comics
-          </NavLink>
-        </li>
-      </ul>
-      <div className="nav-search-cart">
-        <p>search bar</p>
-        <p>cart</p>
-      </div>
-    </nav>
+    <section id="nav-body">
+      <nav id="navbar">
+        <Link to="/ " className="nav-links">
+          <img src={logo} alt="" />
+        </Link>
+        <div className="dropdown-menu" onClick={handleMenuClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <NavLink to="/new-books" className="nav-links">
+              Whats new?
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/categories" className="nav-links">
+              Categories
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/bestsellers" className="nav-links">
+              Bestsellers
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/comics" className="nav-links comic-link">
+              Comics
+            </NavLink>
+          </li>
+        </ul>
+        <div className="nav-search-cart">
+          <p>search bar</p>
+          <p>cart</p>
+        </div>
+      </nav>
+    </section>
   );
 }
