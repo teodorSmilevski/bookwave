ad632b04631b8cceb815824bfd0734ee56b46040
teodorSmilevski <teosmilevski@outlook.com>
2024-04-25 23:16:40 +0200

    Quotes section finished

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -2,10 +2,14 @@ import quotes from "./quotesData";
 export default function Quotes() {
   const randNum = Math.floor(Math.random() * quotes.length);
   return (
-    <section>
-      <h1>{quotes[randNum].author}</h1>
-      <q>{quotes[randNum].quote}</q>
-      <img src={quotes[randNum].image} alt="author" />
+    <section className="quotes">
+      <div>
+        <h2>
+          <q>{quotes[randNum].quote}</q>
+        </h2>
+        <img src={quotes[randNum].image} alt="author" />
+        <h4>{quotes[randNum].author}</h4>
+      </div>
     </section>
   );
 }
