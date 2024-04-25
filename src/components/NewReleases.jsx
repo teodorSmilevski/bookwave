4d287863d5311f7c3904d515113d9f75cb9c8822
teodorSmilevski <teosmilevski@outlook.com>
2024-04-25 23:45:59 +0200

    New Releases section

---
+++ src/components/NewReleases.jsx
@@ -0,0 +1,16 @@
+export default function NewReleases() {
+  return (
+    <section className="new-arrivals">
+      <div className="na-wrapper">
+        <div className="na-heading">
+          <h1>NEW ARRIVALS</h1>
+          <button>SEE ALL</button>
+        </div>
+        <div>
+          <h1>aaa</h1>
+          <h1>nn</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import "../components/HomePage.css";
 import Header from "../components/Header";
 import Quotes from "../components/Quotes";
+import NewReleases from "../components/NewReleases";
 export default function HomePage() {
   return (
     <section>
       <Header />
       <Quotes />
+      <NewReleases />
     </section>
   );
 }
