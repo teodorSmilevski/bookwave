4abee5cda1a47f87afb3f31d27c45695294c360a
teodorSmilevski <teosmilevski@outlook.com>
2024-04-26 21:00:50 +0200

    Comic Section on main page

---
+++ src/components/ComicsFeature.jsx
@@ -0,0 +1,9 @@
+export default function ComicsFeature() {
+  return (
+    <section id="comic-feature">
+      <h1>Need more visuals?</h1>
+      <p>Check this out!</p>
+      <button>See more</button>
+    </section>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import "../components/HomePage.css";
 import Header from "../components/Header";
 import Quotes from "../components/Quotes";
 import NewReleases from "../components/NewReleases";
+import ComicsFeature from "../components/ComicsFeature";
 export default function HomePage() {
   return (
     <section>
       <Header />
       <Quotes />
       <NewReleases />
+      <ComicsFeature />
     </section>
   );
 }
