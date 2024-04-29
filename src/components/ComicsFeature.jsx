f30a99fcc5f930e47ac4c934e95d437141ee67a0
teodorSmilevski <teosmilevski@outlook.com>
2024-04-29 12:44:29 +0200

    Comic Section

--- src/components/ComicsFeature.jsx
+++ src/components/ComicsFeature.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
 export default function ComicsFeature() {
   return (
     <section id="comic-feature">
-      <h1>Need more visuals?</h1>
-      <p>Check this out!</p>
-      <button>See more</button>
+      <div>
+        <div id="cf-text">
+          <h1>
+            Dive into a world of span vibrant visuals and thrilling tales with
+            our comic collection!
+          </h1>
+        </div>
+
+        <Link to="/comics">
+          EXPLORE NOW
+          <i className="bx bx-right-arrow-alt" style={{ color: "#ffffff" }}></i>
+        </Link>
+      </div>
     </section>
   );
 }
