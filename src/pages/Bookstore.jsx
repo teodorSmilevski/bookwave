5cd7579ad4501b8738af6ce40ef41eaa106e7de6
teodorSmilevski <teosmilevski@outlook.com>
2024-05-07 14:23:34 +0200

    Bookstore elements

--- src/pages/Bookstore.jsx
+++ src/pages/Bookstore.jsx
@@ -1,53 +1,23 @@
+import "../components/Bookstore.css";
+import books from "../assets/data/data.json";
+import HomePageCard from "../components/HomePageCard";
 export default function Bookstore() {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        reprehenderit autem voluptatem asperiores possimus saepe aut, non esse
-        blanditiis eligendi voluptas quibusdam! Itaque porro esse sunt aliquid
-        deserunt excepturi, accusantium modi ipsum quisquam quam minima fugit
-        non ex, consequuntur eum soluta sit. Perspiciatis id quod recusandae
-        magni totam blanditiis nesciunt tempore aut. Doloribus et aliquam
-        necessitatibus praesentium nostrum consectetur cumque blanditiis
-        possimus veniam sapiente reiciendis nesciunt, explicabo, assumenda
-        excepturi quaerat animi quam iste quia cupiditate magnam placeat.
-        Aperiam molestiae, eius culpa temporibus rerum distinctio tempora in nam
-        beatae velit aliquid libero quos ex eos quae sit repellat, dolor dolorem
-        deserunt ratione iure corrupti fugiat assumenda cupiditate? Illum totam
-        eius facilis ut atque? Molestiae iste nisi recusandae in quos sapiente
-        eaque accusamus nulla id sed dolore nesciunt asperiores dolor eum
-        aspernatur est, eius quisquam. Fugiat quisquam excepturi qui nemo
-        repudiandae laudantium ullam? Laudantium doloribus debitis id. Maxime
-        enim ad, dolores dolorem soluta nam impedit distinctio itaque nisi,
-        incidunt rerum labore? Odit saepe sapiente rem repudiandae facere,
-        numquam autem, tempore fuga nihil odio dicta recusandae officiis
-        assumenda corrupti ab obcaecati! Fugit vero facilis doloremque atque
-        accusamus sunt, quas corporis libero totam facere impedit esse magni
-        aliquid eaque quisquam cum maiores laboriosam. Officiis itaque
-        perspiciatis nulla non cumque temporibus, laudantium saepe, id eaque
-        reprehenderit illo minus, sed labore nostrum magni consectetur. Rem
-        incidunt accusamus velit asperiores enim repellendus soluta iste hic
-        odio corporis! Nisi explicabo quis consequuntur quasi pariatur minus
-        ullam, facilis error dolorum alias veritatis beatae dolor, illum minima
-        molestiae amet! Fugit commodi quasi vel fuga alias, quia repellat
-        reiciendis voluptates ipsum. Neque vel adipisci quis voluptatibus,
-        dolore placeat quidem reprehenderit, magnam quisquam asperiores modi
-        voluptas commodi natus, totam eveniet voluptate numquam! Architecto
-        autem ipsam maxime accusantium nisi repellendus tempore totam assumenda
-        facilis est maiores inventore possimus aut debitis natus officiis error
-        similique perspiciatis, labore quisquam dolore. Illum omnis, quos
-        quaerat voluptates eveniet qui amet nihil maiores perspiciatis vero
-        sapiente recusandae fugit impedit ipsa, porro laborum odit possimus
-        aperiam deleniti repudiandae doloremque repellat magni expedita! Ipsum,
-        architecto! Quod, officiis, aut, incidunt eveniet nesciunt modi unde non
-        accusantium sit nobis libero neque quibusdam? Nemo voluptates qui
-        eligendi accusantium. Quidem facilis repellendus fuga saepe quisquam
-        assumenda sunt ratione cumque repellat. Sequi officia est molestiae
-        consequuntur, atque, ullam doloribus neque cupiditate ducimus obcaecati
-        alias libero ex dicta quod voluptate. Magni veritatis necessitatibus
-        nisi quisquam adipisci repellendus! Sunt consequuntur est adipisci saepe
-        facilis sit officia inventore.
-      </p>
-    </div>
+    <section id="bookstore">
+      <div id="books-header">sortiraj i naslov</div>
+      <div id="books-wrapper">
+        <div id="filter">filter</div>
+        <div id="books">
+          {books.map((item) => (
+            <HomePageCard
+              key={item.title}
+              title={item.title}
+              author={item.author}
+              img={item.imageLink}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
