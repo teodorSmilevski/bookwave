89a2eb0fbf1a78c6687c88fa5eee6110d0536ab1
teodorSmilevski <teosmilevski@outlook.com>
2024-05-08 23:53:09 +0200

    filter

---
+++ src/components/Filter.jsx
@@ -0,0 +1,10 @@
+export default function Filter() {
+  return (
+    <div id="filter">
+      <div>Genre</div>
+      <div>Price</div>
+      <div>Country</div>
+      <div>Language</div>
+    </div>
+  );
+}
--- src/pages/Bookstore.jsx
+++ src/pages/Bookstore.jsx
@@ -1,12 +1,13 @@
 import "../components/Bookstore.css";
 import books from "../assets/data/data.json";
 import HomePageCard from "../components/HomePageCard";
+import Filter from "../components/Filter";
 export default function Bookstore() {
   return (
     <section id="bookstore">
       <div id="books-header">sortiraj i naslov</div>
       <div id="books-wrapper">
-        <div id="filter">filter</div>
+        <Filter />
         <div id="books">
           {books.map((item) => (
             <HomePageCard
