90dee87b29c52705002d7867f9498b3d773a0585
teodorSmilevski <teosmilevski@outlook.com>
2024-05-15 00:45:39 +0200

    filter

--- src/store/filter-items-context.jsx
+++ src/store/filter-items-context.jsx
@@ -10,91 +10,70 @@ export const FilterContext = createContext({
 
 // eslint-disable-next-line react/prop-types
 export default function FilterContextProvider({ children }) {
-  const [selectedBooks, setSelectedBooks] = useState(books);
-  const [filteredBooks, setFilteredBooks] = useState(selectedBooks);
+  const [filteredBooks, setFilteredBooks] = useState(books);
   const [selectedGenre, setSelectedGenre] = useState([]);
   const [selectedFilter, setSelectedFilter] = useState({
     Price: "",
     Language: "",
-    Country: "",
   });
-  const [selected, setSelected] = useState([]);
   const [sorted, setSorted] = useState("byFeatured");
-  //  TODO: FIX STATE - AKO E BOOKS GI ODBIRA SITE I KO KE SE IZBERI I GENRE I PRICE SE MESHA
-  // TODO: NAJDI RESENIE ZA ZAEDNICKO BARATANJE
+
   useEffect(() => {
-    handleSelectedGenre();
-  }, [selectedGenre]);
-  useEffect(() => {
-    handleSelectedFilter(
-      selectedFilter.Price,
-      selectedFilter.Country,
-      selectedFilter.Language
-    );
-  }, [selectedFilter, selectedBooks]);
-  function handleSelectedGenre() {
+    handleFilters();
+  }, [selectedGenre, selectedFilter, sorted]);
+
+  function handleFilters() {
+    let tempBooks = [...books];
+
+    // Filter by genre
     if (selectedGenre.length > 0) {
-      let tempBooks = selectedGenre.map((bookGenre) => {
-        let temp = books.filter((book) => book.genre === bookGenre);
-        return temp;
-      });
-      setSelectedBooks(tempBooks.flat());
-    } else {
-      setSelectedBooks(books);
-      // TODO: se mesti i dr state
-    }
-  }
-
-  // NOTE: changed genre
-  function handleGenreChange(genre) {
-    if (selectedGenre.includes(genre)) {
-      setSelectedGenre((oldValue) =>
-        oldValue.splice(oldValue.indexOf(genre), 1)
+      tempBooks = tempBooks.filter((book) =>
+        selectedGenre.includes(book.genre)
       );
-    } else {
-      setSelectedGenre((oldValue) => [...oldValue, genre]);
     }
+
+    // Filter by price
+    if (selectedFilter.Price) {
+      let [min, max] = selectedFilter.Price === "0-10" ? [0, 10] : [10, 15];
+      tempBooks = tempBooks.filter(
+        (book) => book.price >= min && book.price <= max
+      );
+    }
+
+    // Filter by language
+    if (selectedFilter.Language) {
+      tempBooks = tempBooks.filter(
+        (book) => book.language === selectedFilter.Language
+      );
+    }
+
+    // Sorting
+    tempBooks.sort((a, b) => {
+      let result;
+      if (sorted === "byFeatured") result = a.id - b.id;
+      else if (sorted === "byPriceLow") result = a.price - b.price;
+      else if (sorted === "byPriceHigh") result = b.price - a.price;
+      return result;
+    });
+
+    setFilteredBooks(tempBooks);
   }
 
-  // NOTE: Selectors
-  function handleSelectChange(item, selectorName) {
-    setSelectedFilter({ ...selectedFilter, [selectorName]: item });
-  }
-  function handleSelectedFilter(price, language) {
-    // FIXME: ERRORI PUKAT BAJO WAWA
-    let tempPrice = [],
-      tempLanguage = [],
-      data = [];
-    price.length > 0 &&
-      (tempPrice = [price].map((price) => {
-        let [min, max] = price === "0-10" ? [0, 10] : [10, 15];
-        let temp = selectedBooks.filter(
-          (book) => book.price >= min && book.price <= max
-        );
-        return temp;
-      }));
-
-    language.length > 0 &&
-      (tempLanguage = [language].map((language) => {
-        let temp = selectedBooks.filter((book) => book.language === language);
-        return temp;
-      }));
-
-    const filteredArray = tempPrice.filter((value) =>
-      tempLanguage.includes(value)
+  function handleGenreChange(genre) {
+    setSelectedGenre((oldValue) =>
+      oldValue.includes(genre)
+        ? oldValue.filter((g) => g !== genre)
+        : [...oldValue, genre]
     );
-    setSelected(filteredArray);
-    console.log(selected);
-    setFilteredBooks(selectedBooks);
   }
 
-  selectedBooks.sort((a, b) => {
-    let result;
-    if (sorted === "byFeatured") result = a.id - b.id;
-    else if (sorted === "byPriceLow") result = a.price - b.price;
-    else if (sorted === "byPriceHigh") result = b.price - a.price;
-    return result;
-  });
+  function handleSelectChange(item, selectorName) {
+    setSelectedFilter((oldFilter) => ({
+      ...oldFilter,
+      [selectorName]: item,
+    }));
+  }
+
   function handleSort(e) {
     setSorted(e.target.value);
   }
