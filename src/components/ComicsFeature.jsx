7e7e63a5ca16c36ebb70b0cf6d88cbdb736a44cb
teodorSmilevski <teosmilevski@outlook.com>
2024-05-30 22:02:23 +0200

    minor changes

--- src/components/ComicsFeature.jsx
+++ src/components/ComicsFeature.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function ComicsFeature() {
   return (
-    <section id="comic-feature" className="container">
+    <section id="comic-feature">
       <div>
         <div id="cf-text">
           <h1>
